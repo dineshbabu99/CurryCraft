045297e3182bfbeda43e4e25e198c293be1221fa
dineshbabu99 <dineshkaru28@gmail.com>
2026-03-27 19:55:32 +0530

    removed unused values

--- src/compoments/pageRoute.tsx
+++ src/compoments/pageRoute.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import {Routes, Route, Router } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Recipe from "../compoments/Recipe";
 
